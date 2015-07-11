85c785ac999c7a3c2e92c46795eebaec4075a11c
Arne Schubert <atd.schubert@gmail.com>
2015-07-11 18:42:04 +0200

    #1: new version with renewed dependencies

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+/*jslint node:true*/
+
 'use strict';
 
 var Socks5ClientHttpAgent = require('./node_modules/socks5-http-client/lib/Agent');
@@ -9,43 +11,46 @@ var defaultTorHost = "localhost";
 var defaultTorPort = 9050;
 
 
-module.exports = function(opts, cb){
-  var proto;
-  var params = {};
-  if (typeof opts === "string") {
-    proto = url.parse(opts).protocol;
-    params.url = opts;
-  }
-  else if(opts.uri){
-    proto = url.parse(opts.uri).protocol;
-    params = opts;
-  } 
-  else if(opts.url){
-    proto = url.parse(opts.url).protocol;
-    params = opts;
-  } else {
-    proto = opts.protocol;
-    params = opts;
-  }
-  
-  var thost = opts.torHost || defaultTorHost;
-  var tport = opts.torPort || defaultTorPort;
-  
-  proto = proto || "http:"; // Fallback
-  
-  delete params.torHost;
-  delete params.torPort;
-  
-  if(proto.indexOf("https")>=0) {
-    params.agent = new Socks5ClientHttpsAgent({
-      socksHost: thost,
-      socksPort: tport
-    });
-  } else {
-    params.agent = new Socks5ClientHttpAgent({
-      socksHost: thost,
-      socksPort: tport
-    });
-  }
-  return request(params, cb);
+module.exports = function (opts, cb){
+    var proto, params, torHost, torPort;
+    params = {};
+    if (typeof opts === "string") {
+        proto = url.parse(opts).protocol;
+        params.url = opts;
+    } else if (opts.uri) {
+        proto = url.parse(opts.uri).protocol;
+        params = opts;
+    } else if (opts.url) {
+        proto = url.parse(opts.url).protocol;
+        params = opts;
+    } else {
+        proto = opts.protocol;
+        params = opts;
+    }
+
+    torHost = opts.torHost || defaultTorHost;
+    torPort = opts.torPort || defaultTorPort;
+
+    proto = proto || "http:"; // Fallback
+
+    delete params.torHost;
+    delete params.torPort;
+
+    if (/https/.test(proto)) {
+        params.agentClass = Socks5ClientHttpsAgent;
+        params.agentOptions = {
+            socksHost: torHost,
+            socksPort: torPort
+        };
+    } else {
+        params.agentClass = Socks5ClientHttpAgent;
+        params.agentOptions = {
+            socksHost: torHost,
+            socksPort: torPort
+        };
+    }
+    if (typeof cb === 'function') {
+        return request(params, cb);
+    }
+    return request(params);
 };
